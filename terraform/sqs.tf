37f5f95755299280f353101cb8daef2b5d803b23
Sara Murphy <smurphy@freshbooks.com>
2022-01-05 20:43:09 -0500

    add IAM policy to queues

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -14,12 +14,61 @@ resource "aws_sqs_queue" "get_instance_info_queue" {
     Candidate = "Sara Angel-Murphy"
   }
 }
+resource "aws_sqs_queue_policy" "get_instance_info_queue_policy" {
+  queue_url = aws_sqs_queue.get_instance_info_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.get_instance_info_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
 
 resource "aws_sqs_queue" "get_instance_info_dl_queue" {
   name = "get_instance_info_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
+}
+
+resource "aws_sqs_queue_policy" "get_instance_info_dl_queue_policy" {
+  queue_url = aws_sqs_queue.get_instance_info_dl_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.get_instance_info_dl_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_sqs_queue" "stop_instance_queue" {
@@ -30,7 +79,7 @@ resource "aws_sqs_queue" "stop_instance_queue" {
   receive_wait_time_seconds  = 0       # Time ReceiveMessage should wait for a message to arrive
   visibility_timeout_seconds = 30      # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.get_instance_info_dl_queue.arn
+    deadLetterTargetArn = aws_sqs_queue.stop_instance_dl_queue.arn
     maxReceiveCount     = 4
   })
 
@@ -39,11 +88,62 @@ resource "aws_sqs_queue" "stop_instance_queue" {
   }
 }
 
+resource "aws_sqs_queue_policy" "stop_instance_queue_policy" {
+  queue_url = aws_sqs_queue.stop_instance_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.stop_instance_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+
 resource "aws_sqs_queue" "stop_instance_dl_queue" {
   name = "stop_instance_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
+}
+
+resource "aws_sqs_queue_policy" "stop_instance_dl_queue_policy" {
+  queue_url = aws_sqs_queue.stop_instance_dl_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.stop_instance_dl_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_sqs_queue" "terminate_instance_queue" {
@@ -62,12 +162,60 @@ resource "aws_sqs_queue" "terminate_instance_queue" {
     Candidate = "Sara Angel-Murphy"
   }
 }
+resource "aws_sqs_queue_policy" "terminate_instance_queue_policy" {
+  queue_url = aws_sqs_queue.terminate_instance_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.terminate_instance_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
 
 resource "aws_sqs_queue" "terminate_instance_dl_queue" {
   name = "terminate_instance_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
+}
+resource "aws_sqs_queue_policy" "terminate_instance_dl_queue_policy" {
+  queue_url = aws_sqs_queue.terminate_instance_dl_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.terminate_instance_dl_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_sqs_queue" "lock_instance_queue" {
@@ -86,10 +234,58 @@ resource "aws_sqs_queue" "lock_instance_queue" {
     Candidate = "Sara Angel-Murphy"
   }
 }
+resource "aws_sqs_queue_policy" "lock_instance_queue_policy" {
+  queue_url = aws_sqs_queue.lock_instance_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.lock_instance_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
 
 resource "aws_sqs_queue" "lock_instance_dl_queue" {
   name = "lock_instance_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
+}
+resource "aws_sqs_queue_policy" "lock_instance_dl_queue_policy" {
+  queue_url = aws_sqs_queue.lock_instance_dl_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.lock_instance_dl_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.instance_id.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
