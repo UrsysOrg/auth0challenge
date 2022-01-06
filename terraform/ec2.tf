0ddbd5a4ee9528bcae2817652b0b965fa3e8526f
Sara Murphy <smurphy@freshbooks.com>
2022-01-06 00:52:22 -0500

    revamp EC2 setup for different sample cases

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,6 @@
+locals {
+  instance_count = 2
+}
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -34,21 +37,91 @@ resource "aws_subnet" "subnet" {
   }
 }
 
-resource "aws_network_interface" "interface" {
-  count       = var.instance_count
-  subnet_id   = aws_subnet.subnet.id
+resource "aws_network_interface" "interface_default" {
+  count     = local.instance_count
+  subnet_id = aws_subnet.subnet.id
   tags = {
-    Name = "primary_network_interface"
+    Candidate = "Sara Angel-Murphy"
   }
 }
 
-resource "aws_instance" "instance" {
-  count         = var.instance_count
+resource "aws_instance" "instance_default" {
+  count         = local.instance_count
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.interface[count.index].id
+    network_interface_id = aws_network_interface.interface_default[count.index].id
+    device_index         = 0
+  }
+  tags = {
+    Candidate = "Sara Angel-Murphy"
+  }
+}
+
+resource "aws_security_group" "open_ssh" {
+  name        = "Open SSH from World"
+  description = "Open SSH from World"
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Candidate = "Sara Angel-Murphy"
+  }
+}
+
+resource "aws_network_interface" "interface_ssh" {
+  count     = local.instance_count
+  subnet_id = aws_subnet.subnet.id
+  tags = {
+    Candidate = "Sara Angel-Murphy"
+  }
+}
+
+resource "aws_instance" "instance_ssh" {
+  count                  = local.instance_count
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.open_ssh.id]
+  tags = {
+    Candidate = "Sara Angel-Murphy"
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.interface_ssh[count.index].id
+    device_index         = 0
+  }
+}
+
+resource "aws_network_interface" "interface_ignore" {
+  count     = local.instance_count
+  subnet_id = aws_subnet.subnet.id
+  tags = {
+    Candidate = "Sara Angel-Murphy"
+  }
+}
+
+resource "aws_instance" "instance_ignore" {
+  count                  = local.instance_count
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.open_ssh.id]
+  tags = {
+      Candidate = "Sara Angel-Murphy",
+      shutdown_service_excluded = "True"
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.interface_ssh[count.index].id
     device_index         = 0
   }
 }
--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -18,9 +18,6 @@ resource "aws_cloudwatch_event_target" "sqs" {
   rule      = aws_cloudwatch_event_rule.instance_id.name
   target_id = "SendToSQS"
   arn       = aws_sqs_queue.get_instance_info_queue.arn
-  tags = {
-    Candidate = "Sara Angel-Murphy"
-  }
   retry_policy {
     maximum_retry_attempts       = "2"
     maximum_event_age_in_seconds = "600"
