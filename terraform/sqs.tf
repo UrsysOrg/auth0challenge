b76a3a0103573cf3fac11c313a22e7fbc5f87009
Sara Murphy <smurphy@freshbooks.com>
2022-01-05 20:03:20 -0500

    Creates eventbridge rule, target, and bus in SQS. Creates 3 sample EC2s for testing.

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,10 +1,10 @@
 resource "aws_sqs_queue" "get_instance_info_queue" {
-  name                      = "get_instance_info_queue"
-  delay_seconds             = 0 # We want to avoid any delays
-  max_message_size          = 262144 # 256k
-  message_retention_seconds = 1209600 # 14 days
-  receive_wait_time_seconds = 0 # Time ReceiveMessage should wait for a message to arrive
-  visibility_timeout_seconds = 30 # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
+  name                       = "get_instance_info_queue"
+  delay_seconds              = 0       # We want to avoid any delays
+  max_message_size           = 262144  # 256k
+  message_retention_seconds  = 1209600 # 14 days
+  receive_wait_time_seconds  = 0       # Time ReceiveMessage should wait for a message to arrive
+  visibility_timeout_seconds = 30      # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.get_instance_info_dl_queue.arn
     maxReceiveCount     = 4
@@ -16,19 +16,19 @@ resource "aws_sqs_queue" "get_instance_info_queue" {
 }
 
 resource "aws_sqs_queue" "get_instance_info_dl_queue" {
-  name                      = "get_instance_info_dl_queue"
+  name = "get_instance_info_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
 }
 
 resource "aws_sqs_queue" "stop_instance_queue" {
-  name                      = "stop_instance_queue"
-  delay_seconds             = 0 # We want to avoid any delays
-  max_message_size          = 262144 # 256k
-  message_retention_seconds = 1209600 # 14 days
-  receive_wait_time_seconds = 0 # Time ReceiveMessage should wait for a message to arrive
-  visibility_timeout_seconds = 30 # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
+  name                       = "stop_instance_queue"
+  delay_seconds              = 0       # We want to avoid any delays
+  max_message_size           = 262144  # 256k
+  message_retention_seconds  = 1209600 # 14 days
+  receive_wait_time_seconds  = 0       # Time ReceiveMessage should wait for a message to arrive
+  visibility_timeout_seconds = 30      # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.get_instance_info_dl_queue.arn
     maxReceiveCount     = 4
@@ -40,19 +40,19 @@ resource "aws_sqs_queue" "stop_instance_queue" {
 }
 
 resource "aws_sqs_queue" "stop_instance_dl_queue" {
-  name                      = "stop_instance_dl_queue"
+  name = "stop_instance_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
 }
 
 resource "aws_sqs_queue" "terminate_instance_queue" {
-  name                      = "terminate_instance_queue"
-  delay_seconds             = 0 # We want to avoid any delays
-  max_message_size          = 262144 # 256k
-  message_retention_seconds = 1209600 # 14 days
-  receive_wait_time_seconds = 0 # Time ReceiveMessage should wait for a message to arrive
-  visibility_timeout_seconds = 30 # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
+  name                       = "terminate_instance_queue"
+  delay_seconds              = 0       # We want to avoid any delays
+  max_message_size           = 262144  # 256k
+  message_retention_seconds  = 1209600 # 14 days
+  receive_wait_time_seconds  = 0       # Time ReceiveMessage should wait for a message to arrive
+  visibility_timeout_seconds = 30      # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.get_instance_info_dl_queue.arn
     maxReceiveCount     = 4
@@ -64,19 +64,19 @@ resource "aws_sqs_queue" "terminate_instance_queue" {
 }
 
 resource "aws_sqs_queue" "terminate_instance_dl_queue" {
-  name                      = "terminate_instance_dl_queue"
+  name = "terminate_instance_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
 }
 
 resource "aws_sqs_queue" "lock_instance_queue" {
-  name                      = "lock_instance_queue"
-  delay_seconds             = 0 # We want to avoid any delays
-  max_message_size          = 262144 # 256k
-  message_retention_seconds = 1209600 # 14 days
-  receive_wait_time_seconds = 0 # Time ReceiveMessage should wait for a message to arrive
-  visibility_timeout_seconds = 30 # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
+  name                       = "lock_instance_queue"
+  delay_seconds              = 0       # We want to avoid any delays
+  max_message_size           = 262144  # 256k
+  message_retention_seconds  = 1209600 # 14 days
+  receive_wait_time_seconds  = 0       # Time ReceiveMessage should wait for a message to arrive
+  visibility_timeout_seconds = 30      # Time in seconds that messages consumed are hidden from other consumers. Adjust depending on average lambda processing time.
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.get_instance_info_dl_queue.arn
     maxReceiveCount     = 4
@@ -88,7 +88,7 @@ resource "aws_sqs_queue" "lock_instance_queue" {
 }
 
 resource "aws_sqs_queue" "lock_instance_dl_queue" {
-  name                      = "lock_instance_dl_queue"
+  name = "lock_instance_dl_queue"
   tags = {
     Environment = "Sara Angel-Murphy"
   }
