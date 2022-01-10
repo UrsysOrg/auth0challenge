48f528c670776cba97f74e4c998f8ec494ac14ac
Sara Murphy <smurphy@freshbooks.com>
2022-01-10 03:21:45 -0500

    linting on conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
-import pytest
 import json
 
-test_instance_id = "i-0c8f8f8f8f8f8f8f8"
-test_region = "us-east-1"
-with open('tests/describe_instance_response.json') as f:
+TEST_INSTANCE_ID = "i-0c8f8f8f8f8f8f8f8"
+TEST_REGION = "us-east-1"
+with open('tests/describe_instance_response.json', encoding='utf-8') as f:
     describe_instance_response = json.load(f)
 
-with open('tests/describe_security_groups_response.json') as f:
+with open('tests/describe_security_groups_response.json', encoding='utf-8') as f:
     describe_security_groups_response = json.load(f)
 
 empty_asg_response = {'AutoScalingInstances': []}
 def test_conf():
+    '''Simply shows that the test config is working'''
     assert True
+    
